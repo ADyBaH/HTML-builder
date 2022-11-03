5f4d2006336014847cd097867b8ed8c1379bf08f
ADyBaH <Brandon4@mail.ru>
2022-11-03 13:28:48 +0300

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const pathFile = path.join(__dirname, 'text.txt');
+let dataText = '';
+
+stdout.write('Привет, пиши сюда:\n');
+stdin.on('data', data => {
+  if (data.toString() === 'exit\r\n') {
+    console.log('Покеда.');
+    process.exit();
+  }
+
+  fs.access(pathFile, (err) => {
+    if (err) {
+      dataText += data.toString();
+      fs.writeFile(
+        pathFile,
+        dataText, (err) => {
+          if (err) throw err;
+        });
+    } else {
+      fs.readFile(pathFile, 'utf-8', (err, file) => {
+        dataText = file;
+        dataText += data.toString();
+        if (err) throw err;
+        fs.writeFile(
+          pathFile,
+          dataText, (err) => {
+            if (err) throw err;
+          });
+      });
+    }
+  });
+});
+process.on('SIGINT', () => {
+  console.log('Покеда.');
+  process.exit();
+});
